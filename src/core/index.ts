ccfdff433e9996b731faaca16453edae85a4dff8
Akuqt <bestor911@gmail.com>
2025-02-19 20:00:55 -0500

    feat: enhance NameCheap class with environment handling and URL construction

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,39 +1,41 @@
-import { ParamsMap, ResponseMap, NameCheapGlobalParams } from "../types";
-import { request, stringifyParams } from "../common";
+import { getURI, initGlobalParams, request, stringifyParams } from "../common";
+import { NameCheapEnv, NameCheapGlobalParams } from "../types";
+import { ParamsMap, ResponseMap } from "../types";
 
 type Params = Omit<NameCheapGlobalParams, "command">;
 type UrlParams<T> = T & NameCheapGlobalParams;
 
 export class NameCheap {
   private globalParams: Params;
+  private env: NameCheapEnv;
 
   constructor(params?: Params) {
-    this.globalParams ??= params || {
-      apiKey: "",
-      apiUser: "",
-      clientIP: "",
-      username: "",
-    };
+    this.globalParams ??= params || initGlobalParams;
+    this.env = "production";
   }
 
   public setGlobalParams(params: Params) {
     this.globalParams = params;
   }
 
+  public setEnv(env: NameCheapEnv) {
+    this.env = env;
+  }
+
   public query<T extends keyof ParamsMap>(
     command: T,
-    params: ParamsMap[T]
+    params: ParamsMap[T],
   ): Promise<ResponseMap[T]> {
     return request<ResponseMap[T]>(
       this.buildURL({
         ...this.globalParams,
         command,
         ...params,
-      })
+      }),
     );
   }
 
   private buildURL<T>(params: UrlParams<T>) {
-    return `https://api.namecheap.com/xml.response?${stringifyParams(params)}`;
+    return `${getURI(this.env)}?${stringifyParams(params)}`;
   }
 }
