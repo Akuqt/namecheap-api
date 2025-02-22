cf65adf7ec045592d99fa0ab6227a6e4c6f39b5c
Akuqt <bestor911@gmail.com>
2025-02-21 19:31:07 -0500

    refactor: simplify index file by exporting NameCheap directly

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { NameCheap } from "./core";
-(async () => {
-  const namecheap = new NameCheap({
-    apiKey: "<...api key...>",
-    apiUser: "user",
-    clientIP: "127.0.0.1",
-    username: "user",
-  });
-
-  const res = await namecheap.query("domains.dns.getHosts", {
-    SLD: "example",
-    TLD: "com",
-  });
-
-  // eslint-disable-next-line no-console
-  console.log(res.ApiResponse.CommandResponse.DomainDNSGetHostsResult);
-})();
+export { NameCheap } from "./core";
